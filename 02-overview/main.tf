331287652be2b4e42bffaa752f18c1144c78e33b
gorkace <gorkace@example.com>
2025-10-29 11:09:27 +0100

    name instances

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -11,12 +11,12 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "exampleA" {
   ami           = "ami-0a716d3f3b16d290c" # Ubuntu latest LTS
   instance_type = "t3.micro"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "exampleB" {
   ami           = "ami-0a716d3f3b16d290c" # Ubuntu latest LTS
   instance_type = "t3.micro"
 }
