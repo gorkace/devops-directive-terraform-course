52a5087519359dedad2bebb8567967e80b9cde7c
gorkace <gorkace@example.com>
2025-10-29 11:38:29 +0100

    ROLLBACK 1 instance

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -11,12 +11,7 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "exampleA" {
-  ami           = "ami-0a716d3f3b16d290c" # Ubuntu latest LTS
-  instance_type = "t3.micro"
-}
-
-resource "aws_instance" "exampleB" {
-  ami           = "ami-0a716d3f3b16d290c" # Ubuntu latest LTS
+resource "aws_instance" "example" {
+  ami           = "ami-0a716d3f3b16d290c" # Ubuntu 20.04 LTS // us-east-1
   instance_type = "t3.micro"
 }
